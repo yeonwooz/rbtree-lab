478cd0a31bdfb187994913e27641f56aa4e0c728
suyeon woo <woosean999@gmail.com>
2022-10-23 14:50:09 +0000

    feature. new_btree

--- src/rbtree.c
+++ src/rbtree.c
@@ -3,8 +3,11 @@
 #include <stdlib.h>
 
 rbtree *new_rbtree(void) {
-  rbtree *p = (rbtree *)calloc(1, sizeof(rbtree));
-  // TODO: initialize struct if needed
+  rbtree *p = (rbtree *)calloc(1, sizeof(rbtree)); //empty tree's root
+  node_t *NIL = (node_t *)calloc(1, sizeof(node_t)); // SENTINEL
+  NIL -> color = RBTREE_BLACK;
+  p -> nil = NIL;
+  p -> root = NIL;
   return p;
 }
 
